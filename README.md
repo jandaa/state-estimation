22e5d7f8152087332da3fe0c8d5baf0958884a92
Andrej <andrej.janda@mail.utoronto.ca>
2022-01-25 10:57:59 -0500

    updates README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # state-estimation
 State estimation assignment re-written in Julia
+
+## Download data
+
+```bash
+wget https://files.ajanda.ca/state-estimation/data.mat
+```
--- src/main.jl
+++ src/main.jl
@@ -4,7 +4,7 @@ include("utils.jl")
 include("estimator.jl")
 
 # Load raw data
-data = matread("dataset3.mat")
+data = matread("data.mat")
 
 # Plot ground truth
 # plot_gt(data)
